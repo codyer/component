25534cbca9ef3b73227f0a57a877b25b9267bdd9
cody.yi <xu.yi@gongbangbang.com>
2019-04-15 22:55:15 +0800

    添加两种常用类型

---
+++ lib-core/src/main/java/com/cody/component/lib/safe/BooleanLiveData.java
@@ -0,0 +1,27 @@
+/*
+ * ************************************************************
+ * 文件：BooleanLiveData.java  模块：lib-core  项目：component
+ * 当前修改时间：2019年04月15日 22:51:24
+ * 上次修改时间：2019年04月15日 22:51:23
+ * 作者：Cody.yi   https://github.com/codyer
+ *
+ * 描述：lib-core
+ * Copyright (c) 2019
+ * ************************************************************
+ */
+
+package com.cody.component.lib.safe;
+
+/**
+ * Created by xu.yi. on 2019/4/15.
+ * component
+ */
+public class BooleanLiveData extends SafeMutableLiveData<Boolean> {
+
+    public void negation() {
+        Boolean old = getValue();
+        if (old != null) {
+            postValue(!old);
+        }
+    }
+}
---
+++ lib-core/src/main/java/com/cody/component/lib/safe/StringLiveData.java
@@ -0,0 +1,20 @@
+/*
+ * ************************************************************
+ * 文件：StringLiveData.java  模块：lib-core  项目：component
+ * 当前修改时间：2019年04月15日 22:53:16
+ * 上次修改时间：2019年04月15日 22:53:16
+ * 作者：Cody.yi   https://github.com/codyer
+ *
+ * 描述：lib-core
+ * Copyright (c) 2019
+ * ************************************************************
+ */
+
+package com.cody.component.lib.safe;
+
+/**
+ * Created by xu.yi. on 2019/4/15.
+ * component
+ */
+public class StringLiveData extends SafeMutableLiveData<String> {
+}
